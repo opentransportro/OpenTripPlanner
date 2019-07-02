0572a8f95c2553c46a977d00ca9776be2e93b6d0
Joel Lappalainen <joel.lappalainen@cgi.com>
2019-07-02 16:23:04 +0300

    Add tests for getPartialElevationProfile

--- src/test/java/org/opentripplanner/routing/util/TestElevationUtils.java
+++ src/test/java/org/opentripplanner/routing/util/TestElevationUtils.java
@@ -5,6 +5,7 @@ import junit.framework.TestCase;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.CoordinateSequence;
 import com.vividsolutions.jts.geom.impl.PackedCoordinateSequenceFactory;
+import org.opentripplanner.common.geometry.PackedCoordinateSequence;
 
 public class TestElevationUtils extends TestCase {
 
@@ -48,5 +49,52 @@ public class TestElevationUtils extends TestCase {
 
 		// 45% downhill hit the MAX_SLOPE_WALK_EFFECTIVE_LENGTH_FACTOR=3 again
 		assertEquals(300.0, ElevationUtils.calculateEffectiveWalkLength(100, -45), 0.1);
+	}
+
+	public void testGetPartialElevationProfile() {
+		int x_length = 10;
+		Coordinate[] c = new Coordinate[6];
+		// [(0, 10), (2, 8), (4, 6), (6, 4), (8, 2), (10, 0)]
+		for (int i = 0; i <= x_length; i += 2) {
+			c[i / 2] = new Coordinate(i, x_length - i);
+		}
+		PackedCoordinateSequence elevationProfile = new PackedCoordinateSequence.Double(c, 2);
+
+		// Test that partial elevation profile from start to finish matched original profile
+		PackedCoordinateSequence fullProfile =
+				ElevationUtils.getPartialElevationProfile(elevationProfile, 0, x_length);
+		assertEquals(elevationProfile.getDimension(), fullProfile.getDimension());
+		assertEquals(elevationProfile.size(), fullProfile.size());
+		assertEquals(elevationProfile.getX(0), fullProfile.getX(0));
+		assertEquals(elevationProfile.getY(0), fullProfile.getY(0));
+		assertEquals(elevationProfile.getX(5), fullProfile.getX(5));
+		assertEquals(elevationProfile.getY(5), fullProfile.getY(5));
+
+		// Test that partial elevation profile from start to half way has same start as full profile
+		// and that its last elevation value is between the closest values from the full profile
+		Double halfWay = 5.0;
+		PackedCoordinateSequence firstHalfProfile =
+				ElevationUtils.getPartialElevationProfile(elevationProfile, 0, halfWay);
+		assertEquals(4, firstHalfProfile.size());
+		assertEquals(elevationProfile.getX(0), firstHalfProfile.getX(0));
+		assertEquals(elevationProfile.getY(0), firstHalfProfile.getY(0));
+		assertEquals(halfWay, firstHalfProfile.getX(3));
+		assertEquals(elevationProfile.getY(3) +
+				(elevationProfile.getY(3) - elevationProfile.getY(4)) / 2,
+				firstHalfProfile.getY(3));
+
+		// Test that partial elevation profile from halfway to end has x values ranging from 0 to
+		// (end - halfway), that the first elevation value is between the closest values from
+		// the full profile and that the end has the same elevation value as full profile's end
+		PackedCoordinateSequence lastHalfProfile =
+				ElevationUtils.getPartialElevationProfile(elevationProfile, halfWay, 10);
+		assertEquals(4, lastHalfProfile.size());
+		assertEquals(0.0, lastHalfProfile.getX(0));
+		assertEquals(elevationProfile.getY(3) +
+						(elevationProfile.getY(3) - elevationProfile.getY(4)) / 2,
+				lastHalfProfile.getY(0));
+		assertEquals(halfWay, lastHalfProfile.getX(3));
+		assertEquals(fullProfile.getY(5),
+				lastHalfProfile.getY(3));
 	}
 }
